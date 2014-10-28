25e74d7218a27a5ea89d4e4b3bdebf34a735d861
Daiki Aminaka <1991.daiki@gmail.com>
2014-10-28 09:24:20 +0900

    implement parsing of fixed header

--- frame.py
+++ frame.py
@@ -1,12 +1,20 @@
 from settings import TYPE
-from binascii import unhexlify
+from binascii import hexlify
 
 def makeHeader(t, dup, qos, retain, length):
     byte1 = hex(t)[2:] + hex(int(str(dup)+bin(qos)[2:].zfill(2)+str(retain), 2))[2:]
     h = hex(length)[2:]
-    byte2 = h.zfill(len(h) + 1 if len(h) % 2 == 1 else len(h))
+    byte2 = "".join([hex(l)[2:].zfill(2) for l in encodeRestLen(length)])
     return byte1 + byte2
-    
+
+def parseHeader(header):
+    byte1 = int(hexlify(header[0]), 16)
+    t = (byte1 & 0xf0) >> 4
+    dub = (byte1 & 0x08) >>3
+    qos = (byte1 & 0x06) >> 1
+    retain = byte1 & 0x01
+    restLen = decodeRestLen([int(hexlify(x), 16) for x in header[1:]])
+    return t, dub, qos, retain, restLen
 
 def encodeRestLen(X):
     output = []

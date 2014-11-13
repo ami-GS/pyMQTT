2d4900d37ecffc8f3a277e389726f75504286f67
Daiki Aminaka <1991.daiki@gmail.com>
2014-11-13 23:22:26 +0900

    change client infomation in server from dictionary to class

--- server.py
+++ server.py
@@ -24,25 +24,14 @@ class Broker():
             while len(data):
                 data = self.sock.recv(1 << 16)
                 fm.parseFrame(data, self)
-                self.clients[self.addr]["timer"].cancel()
-                self.clients[self.addr]["timer"] = Timer(self.clients[self.addr]["keepAlive"]*1.5, self.disconnect)
-                self.clients[self.addr]["timer"].start()
+                self.clients[self.addr].restartTimer() #when DISCONNECT frame coms, then error might occur because the socket is already closed
 
-    def setClient(self, cliID, name, passwd, willTopic, willMessage, keepAlive, clean):
-        self.clients[self.addr] = {"socket":self.sock, "cliID":cliID, "name":name, "passwd":passwd,
-                                   "willTopic":willTopic, "willMessage":willMessage, "clean":clean,
-                                   "keepAlive":keepAlive, "timer":Timer(keepAlive*1.5, self.disconnect),
-                                   "subscribe":[]}
-
-    def setWill(self, topic, message, QoS, retain):
-        self.clients[self.addr]["subscribe"].append([topic, QoS])
-        self.wills[self.clients[self.addr]["cliID"]] = [topics, message, QoS, retain]
-
-    def unsetWill(self):
-        self.wills.pop([self.clients[self.addr]["cliID"]])
+    def setClient(self, cliID, name, passwd, will, keepAlive, clean):
+        self.clients[self.addr] = Client(self, self.addr, self.sock, cliID,
+                                         name, passwd, will, keepAlive, clean)
 
     def setTopic(self, topicQoS, messageID):
-        self.clients[self.addr]["subscribe"].append(topicQoS)
+        self.clients[self.addr].subscribe.append(topicQoS)
 
         if self.clientSubscribe.has_key(topicQoS[0]):
             self.clientSubscribe[topicQoS[0]].append([self.addr, topicQoS[1]])
@@ -53,27 +42,30 @@ class Broker():
         else:
             self.clientSubscribe[topicQoS[0]] = [[self.addr, topicQoS[1]]]
 
+    def unsetWill(self):
+        #when
+        pass
+
+    def sendWill(self, frame):
+        pass # send willFrame to clients ?
+
     def unsetTopic(self, topic):
         # not cool
-        self.clients["subscribe"].remove(self.clients["subscribe"][[t[0] for t in self.clients["subscribe"]].index(topic)])
+        self.clients[self.addr].unsetTopic(topic)
         self.topicQoS[topic].remove(self.topicQoS[[addr[0] for addr in self.TopicQoS[topic]].index(self.addr)])
 
     def disconnect(self):
-        if self.wills.has_key(self.clients[self.addr]["cliID"]):
-            will = self.wills[self.clients[self.addr]["cliID"]]
-            frame = fm.makeFrame(TYPE.PUBLISH, 0, will[2], will[3],
-                                 topic = will[0], message = will[1], messageID = 1)
-            # TODO: send to ??
-        frame = fm.makeFrame(TYPE.DISCONNECT, 0, 0, 0)
-        self.send(frame)
-        self.clients[self.addr]["socket"].close()
-        if self.clients[self.addr]["clean"]:
+        # when get DISCONNECT packet from client
+        self.clients[self.addr].sock.close()
+        if self.clients[self.addr].clean:
+            # TODO: correct ?
             self.clients.pop(self.addr)
         print "disconnect"
 
     def suback(self, messageID):
+        # this looks mistake, the qosList should contain only subscribed QoSs
         frame = fm.makeFrame(TYPE.SUBACK, 0, 0, 0, messageID = messageID,
-                             qosList = [topic[1] for topic in self.clients[self.addr]["subscribe"]])
+                             qosList = [topic[1] for topic in self.clients[self.addr].subscribe])
         self.send(frame)
 
     def unsuback(self, messageID):
@@ -101,15 +93,14 @@ class Broker():
         self.send(frame)
 
     def pingresp(self):
-        frame = fm.makeFrame(TYPE.PINGRESP, 0, 0, 0)
-        self.send(frame)
+        self.clients[self.addr].pingresp()
 
     def publish(self, topic, message, messageID = 1, retain = 0):
         if self.topics.has_key(topic):
             for client in self.topics[topic]:
                 frame = fm.makeFrame(TYPE.PUBLISH, 0, client[1], 0, topic = topic,
                                      message = message, messageID = messageID)
-                self.clients[client[0]]["socket"].send(frame) # TODO: send function should be unified
+                self.clients[client[0]].send(frame) # TODO: send function should be unified
         else:
             self.topics[topic] = ""
 
@@ -126,4 +117,49 @@ class Broker():
         self.send(frame)
 
     def send(self, frame):
-        self.clients[self.addr]["socket"].send(frame)
+        self.clients[self.addr].send(frame)
+
+class Client():
+    def __init__(self, server, addr, sock, cliID = "", name = "",
+                 passwd = "", will = {}, keepAlive = 2, clean = 1):
+        self.server = server
+        self.addr = addr
+        self.sock = sock
+        if not cliID:
+            cliID = "random" # TODO: cliID should be determined in here if no cliID was delivered.
+        self.cliID = cliID
+        self.name = name
+        self.passwd = passwd
+        self.will = will
+        self.keepAlive = keepAlive
+        self.timer = Timer(keepAlive * 1.5, self.disconnect)
+        self.subscribe = []
+        self.cleanSession = clean
+
+    def sendWill(self, frame):
+        self.server.sendWill(frame)
+
+    def disconnect(self):
+        # when ping packet didn't came within the keepAlive * 1.5 sec
+        frame = fm.makeFrame(TYPE.PUBLISH, 0, self.will["QoS"], self.will["retain"],
+                             topic = self.will["topic"], message = self.will["message"], messageID = 1)
+        self.sendWill(frame)
+        frame = fm.makeFrame(TYPE.DISCONNECT, 0, 0, 0)
+        self.send(frame)
+        self.sock.close()
+        self.server.clients.pop(self.addr)
+
+    def pingresp(self):
+        frame = fm.makeFrame(TYPE.PINGRESP, 0, 0, 0)
+        self.send(frame)
+
+    def unsetTopic(self, topic):
+        self.subscribe.remove(self.subscribe[[t[0] for t in self.subscribe].index(topic)])
+
+    def send(self, frame):
+        self.sock.send(frame)
+
+    def restartTimer(self):
+        self.timer.cancel()
+        self.timer = Timer(self.keepAlive * 1.5, self.disconnect)
+        self.timer.start()
